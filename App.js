c6e11b84dc84b9840ed0595b1d5e07d42ea95bd3
wadeed.siddiqui <wadeed.siddiqui@pral.com.pk>
2021-12-19 13:26:55 +0500

    implemented post

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
@@ -21,6 +23,16 @@ app.get('/api/courses/:id', (req, res) => {
     if(!course) res.status(404).send('The course with the given id was not found');
     res.send(course);
 });
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+})
 
 
 
