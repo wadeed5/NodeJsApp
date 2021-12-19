63b3e65089356862a272c44f5809f761acb99ffb
wadeed.siddiqui <wadeed.siddiqui@pral.com.pk>
2021-12-19 13:07:02 +0500

    added env variable PORT

--- App.js
+++ App.js
@@ -11,4 +11,6 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+// PORT (Env Variable)
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
