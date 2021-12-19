6365a2d3fa73664cd9ad4850c59e3b21d0c74862
wadeed.siddiqui <wadeed.siddiqui@pral.com.pk>
2021-12-19 13:20:10 +0500

    implemented get

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const app = express();
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
 
 
 app.get('/', (req, res) => {
@@ -8,8 +13,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given id was not found');
+    res.send(course);
+});
+
+
 
 // PORT (Env Variable)
 const port = process.env.PORT || 3000;
